d79c207af4023b2b7ebf233c35db94c0100d7af4
saman <sam.malakjan.com>
2025-12-22 16:10:09 +0330

    fix:remove js files that showed only mandatory field in employee and customer

--- foundation/public/js/customer-test.js
+++
@@ -1,9 +0,0 @@
-// foundation/public/js/customer_mandatory.js
-
-console.log("[customer_mandatory] file loaded");
-
-frappe.ui.form.on("Customer", {
-    refresh(frm) {
-        console.log("[customer_mandatory] refresh for", frm.doctype, frm.doc.name);
-    },
-});
--- foundation/public/js/employee-test.js
+++
@@ -1,9 +0,0 @@
-// foundation/public/js/employee_mandatory.js
-
-console.log("[employee_mandatory] file loaded");
-
-frappe.ui.form.on("Employee", {
-    refresh(frm) {
-        console.log("[employee_mandatory] refresh for", frm.doctype, frm.doc.name);
-    },
-});
